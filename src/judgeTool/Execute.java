776d16cf8902fdc4251bf18a82c59115f9a0ed54
Cloud Creator <qingyun_yang@foxmail.com>
2022-11-12 22:40:40 +0800

    fix bug

--- src/judgeTool/Execute.java
+++ src/judgeTool/Execute.java
@@ -7,11 +7,17 @@ public class Execute {
     
     private File excutableFile;
 
-    Execute(File codeFile, File codeDir, File outputDir) throws IOException, InterruptedException {
-        Process p = Runtime.getRuntime().exec("g++ "+codeFile.getName()+" "+outputDir.getPath()+"/"+
+    Execute(File codeFile, File codeDir, File outputDir) throws IOException {
+        Process p = Runtime.getRuntime().exec("g++ "+codeFile.getName()+" -o "+outputDir.getCanonicalPath()+"/"+
                 codeFile.getName().substring(0, codeFile.getName().lastIndexOf(".")), null, codeDir);
-        if(p.waitFor() != 0) {
-            throw new IOException("It seems like g++ not exit properly :( error code: "+p.exitValue());
+        try {
+            if(p.waitFor() != 0) {
+                excutableFile = null;
+            }
+        }
+        catch(InterruptedException e) {
+            System.out.println("Unknown error: "+e);
+            System.exit(-1);
         }
         this.excutableFile = new File(outputDir.getPath()+"/"+
                 codeFile.getName().substring(0, codeFile.getName().lastIndexOf(".")));
