460244697baf123266a8700edea370a6ef56be61
Cloud Creator <qingyun_yang@foxmail.com>
2022-11-12 23:01:33 +0800

    exec program to judge

--- src/judgeTool/Execute.java
+++ src/judgeTool/Execute.java
@@ -6,10 +6,14 @@ import java.io.IOException;
 public class Execute {
     
     private File excutableFile;
+    private File outputFile;
 
     Execute(File codeFile, File codeDir, File outputDir) throws IOException {
         Process p = Runtime.getRuntime().exec("g++ "+codeFile.getName()+" -o "+outputDir.getCanonicalPath()+"/"+
                 codeFile.getName().substring(0, codeFile.getName().lastIndexOf(".")), null, codeDir);
+        outputFile = new File(codeFile.getName()+" -o "+outputDir.getCanonicalPath()+"/"+
+                codeFile.getName().substring(0, codeFile.getName().lastIndexOf("."))+".txt");
+        outputFile.createNewFile();
         try {
             if(p.waitFor() != 0) {
                 excutableFile = null;
@@ -23,7 +27,17 @@ public class Execute {
                 codeFile.getName().substring(0, codeFile.getName().lastIndexOf(".")));
     }
 
-    void exec(String testcase) {
-        
+    void exec(String testcase) throws IOException {
+        Process p = Runtime.getRuntime().exec("echo \""+testcase+"\" | "+excutableFile.getCanonicalPath()+
+                    " >> "+outputFile.getCanonicalPath(), null);
+        Runtime.getRuntime().exec("echo \"\" >> "+outputFile.getCanonicalPath());
+        try {
+            if(p.waitFor() != 0) {
+                Runtime.getRuntime().exec("echo \"Error: \"" +p.exitValue()+ " >> "+outputFile.getCanonicalPath());
+            }
+        }
+        catch(InterruptedException e) {
+            Runtime.getRuntime().exec("echo \"InterruptedException\" >> "+outputFile.getCanonicalPath());
+        }
     }
 }
