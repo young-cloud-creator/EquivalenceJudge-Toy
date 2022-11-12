0e61cef8f2c3a152cf7697eb561e0bb5beb183bd
Cloud Creator <qingyun_yang@foxmail.com>
2022-11-12 16:39:59 +0800

    parse input format

---
+++ src/judgeTool/InputGenerator.java
@@ -0,0 +1,115 @@
+package judgeTool;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class InputGenerator {
+    private final File inFormat;
+    private final ArrayList<InType> inputTypes;
+    private boolean knowInputTypes;
+
+    InputGenerator(File f) {
+        this.inFormat = f;
+        this.inputTypes = new ArrayList<>();
+        this.knowInputTypes = false;
+    }
+
+    String genInput() {
+        String inputString = "";
+        if(!knowInputTypes) {
+            getInputTypes();
+        }
+        for(InType inType : inputTypes) {
+            switch(inType.getInType()) {
+                case INT:
+                    break;
+                case CHAR:
+                    break;
+                case STRING:
+                    break;
+            }
+        }
+        return inputString;
+    }
+
+    private void getInputTypes() {
+        this.knowInputTypes = true;
+        if(this.inFormat == null) {
+            return;
+        }
+        else {
+            try (Scanner inScanner = new Scanner(this.inFormat)) {
+                while(inScanner.hasNext()) {
+                    String exp = inScanner.next();
+                    InType result = parseExp(exp);
+                    if(result != null) {
+                        this.inputTypes.add(result);
+                    }
+                }
+            }
+            catch(FileNotFoundException e) {}
+        }
+    }
+
+    private InType parseExp(String exp) {
+        String intContent = "int\\((\\+|-)?[0-9]+,(\\+|-)?[0-9]+\\)";
+        String charContent = "char";
+        String stringContent = "string\\((\\+)?[0-9]+,(\\+)?[0-9]+\\)";
+        String rangeContent = "((\\+|-)?[0-9]+)((\\+|-)?[0-9]+)";
+        Pattern rangePattern = Pattern.compile(rangeContent);
+        Matcher rangeMatcher = rangePattern.matcher(exp);
+        rangeMatcher.find();
+
+        if(Pattern.matches(intContent, exp)) {
+            return new InType(Types.INT, Integer.parseInt(rangeMatcher.group(1)), 
+                                        Integer.parseInt(rangeMatcher.group(2)));
+        }
+        else if(Pattern.matches(charContent, exp)) {
+            return new InType(Types.CHAR, 0, 0);
+        }
+        else if(Pattern.matches(stringContent, exp)) {
+            return new InType(Types.STRING, Integer.parseInt(rangeMatcher.group(1)), 
+                                        Integer.parseInt(rangeMatcher.group(2)));
+        }
+        else {
+            return null;
+        }
+    }
+
+    private enum Types {
+        INT,
+        CHAR,
+        STRING;
+    }
+    
+    private class InType {
+
+        private final Types inType;
+        private final int low;
+        private final int up;
+
+        InType(Types inType, int low, int up) {
+            this.inType = inType;
+            this.low = low;
+            this.up = up;
+        }
+
+        Types getInType() {
+            return inType;
+        }
+
+        int getLow() {
+            return low;
+        }
+
+        int getUp() {
+            return up;
+        }
+
+    }
+
+}
