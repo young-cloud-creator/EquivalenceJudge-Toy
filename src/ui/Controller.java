39730079b3cd3a23e675ec10bdfc577e3852a032
Cloud Creator <qingyun_yang@foxmail.com>
2022-11-13 16:11:09 +0800

    output result to csv file

--- src/ui/Controller.java
+++ src/ui/Controller.java
@@ -1,7 +1,10 @@
 package ui;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
+
 import dataStructure.UFS;
 import judgeTool.JudgeEquivalence;
 
@@ -30,13 +33,39 @@ public class Controller {
             judge.judge();
             UFS resultUFS = judge.getJudgeResult();
             File[] resultFiles = judge.getFiles();
-            processResult(resultUFS, resultFiles);
+            processResult(resultUFS, resultFiles, subDir);
         }
     }
 
-    private void processResult(UFS equivalence, File[] files) {
-        // TODO: process the UFS and write file pairs to .csv files
+    private void processResult(UFS equivalence, File[] files, File subDir) throws IOException {
+        // process the UFS and write file pairs to .csv files
         // TODO: convert UFS to String[][] so that it can be output by MainUI
+        output2csv(equivalence, files, subDir);
+    }
+
+    private void output2csv(UFS equivalence, File[] files, File subDir) throws IOException {
+        File equalCSV = new File(subDir.getCanonicalPath()+"/"+"equal.csv");
+        File inequalCSV = new File(subDir.getCanonicalPath()+"/"+"inequal.csv");
+        equalCSV.createNewFile();
+        inequalCSV.createNewFile();
+        FileOutputStream equalFos = new FileOutputStream(equalCSV);
+        FileOutputStream inequalFos = new FileOutputStream(inequalCSV);
+        OutputStreamWriter equalWriter = new OutputStreamWriter(equalFos);
+        OutputStreamWriter inequalWriter = new OutputStreamWriter(inequalFos);
+
+        for(int i=0; i<files.length; i++) {
+            for(int j=i+1; j<files.length; j++) {
+                if(equivalence.isSameRoot(i, j)) {
+                    equalWriter.write(files[i].getCanonicalPath()+","+files[j].getCanonicalPath()+"\n");
+                }
+                else {
+                    inequalWriter.write(files[i].getCanonicalPath()+","+files[j].getCanonicalPath()+"\n");
+                }
+            }
+        }
+
+        equalWriter.close();
+        inequalWriter.close();
     }
 
 }
