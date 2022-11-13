57ffe128d54fd863c98b87d546f587eccf6581c0
Cloud Creator <qingyun_yang@foxmail.com>
2022-11-13 14:35:01 +0800

    fix bug

--- src/judgeTool/Execute.java
+++ src/judgeTool/Execute.java
@@ -6,42 +6,51 @@ import java.io.IOException;
 public class Execute {
     
     private File excutableFile;
-    private File outputFile;
+    private final File outputFile;
+    private final String shell;
 
     Execute(File codeFile, File codeDir, File outputDir) throws IOException {
-        Process p = Runtime.getRuntime().exec("g++ "+codeFile.getName()+" -o "+outputDir.getCanonicalPath()+"/"+
-                codeFile.getName().substring(0, codeFile.getName().lastIndexOf(".")), null, codeDir);
-        outputFile = new File(codeFile.getName()+" -o "+outputDir.getCanonicalPath()+"/"+
+        this.shell = System.getenv("SHELL");
+        Process p = Runtime.getRuntime().exec(new String[]{this.shell, "-c",
+            "g++ "+codeFile.getName()+" -o "+outputDir.getCanonicalPath()+"/"+
+                codeFile.getName().substring(0, codeFile.getName().lastIndexOf("."))}, null, codeDir);
+        outputFile = new File(outputDir.getCanonicalPath()+"/"+
                 codeFile.getName().substring(0, codeFile.getName().lastIndexOf("."))+".txt");
         outputFile.createNewFile();
         try {
             if(p.waitFor() != 0) {
                 excutableFile = null;
             }
+            else {
+                this.excutableFile = new File(outputDir.getPath()+"/"+
+                codeFile.getName().substring(0, codeFile.getName().lastIndexOf(".")));
+            }
         }
         catch(InterruptedException e) {
             System.out.println("Unknown error: "+e);
             System.exit(-1);
         }
-        this.excutableFile = new File(outputDir.getPath()+"/"+
-                codeFile.getName().substring(0, codeFile.getName().lastIndexOf(".")));
     }
 
     void exec(String testcase) throws IOException {
         if(excutableFile == null) {
-            Runtime.getRuntime().exec("echo \"can't compile\" >> "+outputFile.getCanonicalPath());
+            Runtime.getRuntime().exec(new String[]{this.shell, "-c",
+                "echo \"can't compile\" >> "+outputFile.getCanonicalPath()});
         }
         else {
-            Process p = Runtime.getRuntime().exec("echo \""+testcase+"\" | "+excutableFile.getCanonicalPath()+
-                        " >> "+outputFile.getCanonicalPath(), null);
-            Runtime.getRuntime().exec("echo \"\" >> "+outputFile.getCanonicalPath());
+            Process p = Runtime.getRuntime().exec(new String[]{this.shell, "-c",
+                "echo \""+testcase+"\" | "+excutableFile.getCanonicalPath()+" >> "+outputFile.getCanonicalPath()});
+            Runtime.getRuntime().exec(new String[]{this.shell, "-c",
+                "echo \"\" >> "+outputFile.getCanonicalPath()});
             try {
                 if(p.waitFor() != 0) {
-                    Runtime.getRuntime().exec("echo \"Nonzero Error\" >> "+outputFile.getCanonicalPath());
+                    Runtime.getRuntime().exec(new String[]{this.shell, "-c",
+                        "echo \"Nonzero Error\" >> "+outputFile.getCanonicalPath()});
                 }
             }
             catch(InterruptedException e) {
-                Runtime.getRuntime().exec("echo \"InterruptedException\" >> "+outputFile.getCanonicalPath());
+                Runtime.getRuntime().exec(new String[]{this.shell, "-c",
+                    "echo \"InterruptedException\" >> "+outputFile.getCanonicalPath()});
             }
         }
     }
